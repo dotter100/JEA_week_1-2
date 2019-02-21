650a6159dc624f3af69f9e1f4e6882280b60cdf6
BJBaardse <beer.baardse@gmail.com>
2019-02-21 16:01:33 +0100

    Interceptor works

---
+++ IDEA/src/java/model/TracingInterceptor.java
@@ -0,0 +1,14 @@
+package model;
+
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.InvocationContext;
+
+public class TracingInterceptor {
+
+    @AroundInvoke
+    public Object logCall(InvocationContext context) throws Exception{
+        System.out.println("Invoking method: " + context.getMethod());
+        return context.proceed();
+    }
+}
+ 
